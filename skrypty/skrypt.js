208e1c06a8fa7224d745bd61479f97495835ec6e
LoriV2 <94517486+LoriV2@users.noreply.github.com>
2023-10-03 13:25:36 +0200

    rzeczy

--- skrypty/skrypt.js
+++
@@ -1,9 +0,0 @@
-function showCustomMenu(){
-    document.getElementById("menu").hidden = false;
-}
-
-window.oncontextmenu = function ()
-{
-    showCustomMenu();
-    return false;     // cancel default menu
-}
