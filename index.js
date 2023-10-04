760378f3be1503eff307aca0147d707c1fef1652
Lorenzo <94517486+LoriV2@users.noreply.github.com>
2023-10-05 00:10:18 +0200

    rzeczy

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.json());
-app.use(express.static("express"));
+app.use(express.static("strona"));
+app.set('view engine', 'ejs');
 // default URL for website
 app.route('/')
   .all(function (req, res, next) {
-    res.sendFile(path.join(__dirname + '/podstrony/index.html'));
+    res.render(path.join(__dirname + '/podstrony/index.ejs'), { data: "jej" });
   });
+app.route('/Nartykuly')
+  .all(function (req, res, next) {
+    res.render(path.join(__dirname + '/podstrony/Narykuly.ejs'));
+  });
+
 const hostname = 'localhost';
 const port = 3000;
 
