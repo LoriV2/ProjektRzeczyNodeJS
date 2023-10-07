f959ae83d8047974a3fa15d8d0eeaa2f6ae2c0b5
Lorenzo <94517486+LoriV2@users.noreply.github.com>
2023-10-07 16:19:59 +0200

    trochę pozmieniałem

--- index.js
+++ index.js
@@ -3,36 +3,32 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.json());
-app.use(express.static("strona"));
-app.set('view engine', 'ejs');
-// default URL for website
+app.use(express.static('views'));
+app.set('view engine', 'pug');
 
 const PORT = process.env.PORT || 3030;
+
+app.route('/')
+  .all(function (req, res, next) {
+    res.render('index', { title: 'Strona Główna', message: 'Hello there!' });
+  });
+app.route('/Nartykuly')
+  .all(function (req, res, next) {
+    res.render('Narykuly', { title: 'Hey', message: 'Hello there!' });
+  });
+
+
+app.use((req, res, next) => {
+  res.status(404).send("Nie ma czegoś takiego!")
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Coś sie popsuło!!!')
+});
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
-
-app.route('')
-  .all(function (req, res, next) {
-    res.status(200).render(path.join(__dirname + '/podstrony/index.ejs'), { data: "jej" });
-  });
-app.route('/Nartykuly')
-  .all(function (req, res, next) {
-    res.status(200).render(path.join(__dirname + '/podstrony/Narykuly.ejs'));
-  });
-
-
-
-app.route('/skrypty/skrypt.js')
-  .all(function (req, res, next) {
-    res.status(200).sendFile(path.join(__dirname + '/skrypty/skrypt.js'));
-  });
-
-app.route('/css/css.css')
-  .all(function (req, res, next) {
-    res.status(200).sendFile(path.join(__dirname + '/css/css.css'));
-  });
-
 
 
