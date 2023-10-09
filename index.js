74733c9d2df1378360e5611c487fd1050325f871
Lorenzo <94517486+LoriV2@users.noreply.github.com>
2023-10-09 18:43:05 +0200

    połączenie z bazą danych

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const http = require('http');
+require('dotenv').config();
 const express = require('express');
-const path = require('path');
 const app = express();
+var sql = require('mssql');
+const fs = require('fs');
+
+// Wczytaj dane z pliku .json
+const rawData = fs.readFileSync('rzeczy.json', 'utf8');
+var dbconfig = JSON.parse(rawData);
+
+const pool = new sql.ConnectionPool(dbconfig);
+pool.connect()
+  .then((pool) => {
+    console.log('Połączono z bazą danych.');
+  })
+  .catch((err) => {
+    console.error('Błąd połączenia z bazą danych:', err);
+  });
+
 app.use(express.json());
 app.use(express.static('views'));
 app.set('view engine', 'pug');
@@ -10,11 +25,20 @@ const PORT = process.env.PORT || 3030;
 
 app.route('/')
   .all(function (req, res, next) {
+    let query = "Select TOP 4 * From artykuly ORDER BY data_dodania";
     res.render('index', { title: 'Strona Główna', message: 'Hello there!' });
-  });
+  }
+
+  )
+  .post(function (req, res, next) {
+    let query = "Select TOP 4 * From artykuly ORDER BY data_dodania";
+
+  })
+
+  ;
 app.route('/Nartykuly')
   .all(function (req, res, next) {
-    res.render('Narykuly', { title: 'Hey', message: 'Hello there!' });
+    res.render('Narykuly', { title: 'Najnowsze artykuły', message: 'Hello there!' });
   });
 
 
