9171c80a462d0e9613d928cc05c835a95c420e79
Lorenzo <94517486+LoriV2@users.noreply.github.com>
2023-10-10 07:50:56 +0200

    z

--- index.js
+++ index.js
@@ -1,12 +1,66 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+//serwer połączenie
 var sql = require('mssql');
+//pliki!!
 const fs = require('fs');
-
 // Wczytaj dane z pliku .json
 const rawData = fs.readFileSync('rzeczy.json', 'utf8');
 var dbconfig = JSON.parse(rawData);
+//do szyfrowania
+const crypto = require('crypto');
+const secretKey = crypto.randomBytes(32).toString('hex');
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+app.use(session({ secret: secretKey, resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+async function connectDatabase(querry) {
+  try {
+    await sql.connect(dbconfig);
+    console.log('Połączono z bazą danych');
+
+    // Wykonaj zapytanie
+    const result = await sql.query(querry);
+    return result.recordset;
+  } catch (error) {
+    console.error('Błąd połączenia z bazą danych:', error);
+  } finally {
+    // Zamknij połączenie po użyciu
+    sql.close();
+  }
+}
+
+// Wywołaj funkcję łączenia z bazą danych
+
+// Skonfiguruj Passport i zdefiniuj strategię lokalną
+passport.use(new LocalStrategy(
+  (username, password, done) => {
+    connectDatabase('SELECT * FROM uzytkownicy WHERE Haslo_uz = ' + password + ' && Login_uz = ' + username + ' ');
+    // Sprawdź, czy użytkownik o podanym loginie i haśle istnieje w bazie danych
+    // Jeśli uwierzytelnienie jest pomyślne, wywołaj done(null, user);
+    // W przeciwnym razie, wywołaj done(null, false);
+  }
+));
+
+// Serializuj i deserializuj użytkownika
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  // Pobierz użytkownika na podstawie ID
+  // Jeśli użytkownik istnieje, wywołaj done(null, user);
+  // W przeciwnym razie, wywołaj done(null, null);
+});
+
+
 
 const pool = new sql.ConnectionPool(dbconfig);
 pool.connect()
@@ -26,16 +80,19 @@ const PORT = process.env.PORT || 3030;
 app.route('/')
   .all(function (req, res, next) {
     let query = "Select TOP 4 * From artykuly ORDER BY data_dodania";
-    res.render('index', { title: 'Strona Główna', message: 'Hello there!' });
+    res.render('index', { title: 'Strona Główna', message: connectDatabase("SELECT * FROM uzytkownicy") });
   }
 
   )
   .post(function (req, res, next) {
     let query = "Select TOP 4 * From artykuly ORDER BY data_dodania";
 
-  })
+  });
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
 
-  ;
 app.route('/Nartykuly')
   .all(function (req, res, next) {
     res.render('Narykuly', { title: 'Najnowsze artykuły', message: 'Hello there!' });
